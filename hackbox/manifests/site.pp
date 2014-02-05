8f95afa6aefd6689dddc2a9964adc87a50a129c5
Michael Krieg <info@red-tag.de>
2014-02-05 10:51:08 +0100

    Hostname und Zeitzone korrekt setzen in der Hackbox

--- hackbox/manifests/site.pp
+++ hackbox/manifests/site.pp
@@ -3,5 +3,20 @@ Exec {
 }
 
 node default {
+  exec { "update_timezone":
+     command => "echo 'Europe/Berlin' > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata",
+     unless  => "grep Berlin /etc/timezone",
+  }
+  
+  exec { 'update_hostname':
+    command => "echo 'phpughalle-hackbox' > /etc/hostname && service hostname.sh start",
+    unless  => "grep 'phpughalle-hackbox' /etc/hostname",
+  }
+
+  host { 'phpughalle-hackbox.local':
+    ip           => $ipaddress_eth1,
+    host_aliases => [ 'phpughalle-hackbox', 'hackbox' ],
+    notify       => Exec['update_hostname'],
+  }
 }
 
