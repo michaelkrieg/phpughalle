3e60406826a0ece915c5f5afde510065a14317e6
Michael Krieg <info@red-tag.de>
2014-02-05 13:26:38 +0100

    je eine Box für PHP 5.4 und 5.5

--- hackbox/manifests/site.pp
+++ hackbox/manifests/site.pp
@@ -1,26 +1,7 @@
-Exec {
-  path  => join(['/usr/bin', '/usr/sbin', '/bin', '/sbin'], ':')
-}
 
-node default {
-  exec { "update_timezone":
-     command => "echo 'Europe/Berlin' > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata",
-     unless  => "grep Berlin /etc/timezone",
-  }
-  
-  exec { 'update_hostname':
-    command => "echo 'phpughalle-hackbox' > /etc/hostname && service hostname.sh start",
-    unless  => "grep 'phpughalle-hackbox' /etc/hostname",
-  }
 
-  host { 'phpughalle-hackbox.local':
-    ip           => $ipaddress_eth1,
-    host_aliases => [ 'phpughalle-hackbox', 'hackbox' ],
-    notify       => Exec['update_hostname'],
-  }
 
-  package { ['git', 'ant', 'acl', 'curl', 'sudo', 'screen']:
-    ensure => latest,
-  }
-}
+class { 'hackbox': }
+
+
 
